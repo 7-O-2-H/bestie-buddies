0ab8d8edc00ef3714ed3b3aa97afca27e6f9abe8
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-06-03 22:41:03 -0400

    add navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBone } from '@fortawesome/free-solid-svg-icons';
+
+
+export default function Navbar() {
+
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  //const navigate = useNavigate();
+  //const isLoggedIn = JSON.parse(localStorage.getItem("loggedin"));
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  // const handleLogout = () => {
+  //   localStorage.removeItem("loggedin");
+  //   localStorage.removeItem("user_id");
+  //   navigate("/login");
+  // };
+
+  return (
+
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="bestie">
+        <div>
+          bestie <FontAwesomeIcon icon={faBone}/>
+        </div> 
+        <div>
+          BUDDIES
+        </div>
+        
+      </div>
+      <div className="toggleButton">
+        <button onClick={() => {
+          setExpandNavbar((prev) => !prev);
+        }}>
+        
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/about"> ABOUT </Link>
+        {/* {isLoggedIn && (
+      <Link to="/sampleprojects">Portfolio</Link>
+    )} */}
+      <Link to="/contact"> CONTACT </Link>
+      {/* {isLoggedIn ? (
+      <button onClick={handleLogout}>Logout</button> */}
+    {/* ) : ( */}
+      <>
+        <Link to="/login"> LOGIN </Link>
+        <Link to="/registration">SIGNUP</Link>
+      </>
+      </div>
+    </div>
+  );
+};
